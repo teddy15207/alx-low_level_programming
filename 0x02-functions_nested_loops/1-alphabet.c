62eb47a35a61017e9fd6d7ba43fa4360341d69c1
tewodros <teddy165207@gmail.com>
2022-04-13 17:32:28 +0300

    my 2nd task commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * main -main block
- * Description: print - the alphabet, in lowercase, followed by a new line
+ *print_alphabe  -prints the alphabet, in lowercase, followed by a new line
  */
 void print_alphabet(void)
 {
