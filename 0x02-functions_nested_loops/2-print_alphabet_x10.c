33df97f7e3c6abcc2c73fa76a6d4a85abdd6d68e
tewodros <teddy165207@gmail.com>
2022-04-14 13:10:32 +0300

    my 3rd task commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line.
  * Return : always 0
  */
 void print_alphabet_x10(void)
