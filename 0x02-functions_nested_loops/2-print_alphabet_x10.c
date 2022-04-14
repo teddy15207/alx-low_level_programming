6bcd7cb8b79bb3ec1106a88d3cfb43fdebda3f35
tewodros <teddy165207@gmail.com>
2022-04-14 08:39:14 +0300

    my 3rd task commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char c;
+	int count = 0;
+	char letter;
 
-	while (i = 0; i < 10; i++)
+
+	while (count++ <= 9)
 	{
-	for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
+	for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		        _putchar('\n');
 	}
 }
